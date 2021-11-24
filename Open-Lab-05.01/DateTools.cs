227a1c28083a4bfb9c67442880a136ad8aac4db4
smidovic <msmidovic@gmail.com>
2021-11-24 19:16:18 +0100

    storocie

--- Open-Lab-05.01/DateTools.cs
+++ Open-Lab-05.01/DateTools.cs
@@ -6,7 +6,62 @@ namespace Open_Lab_05._01
     {
         public string Century(int year)
         {
-            throw new NotImplementedException();
+            if (year%100 == 00 )
+            {
+                int rok = year / 100;
+
+                if (year == 1)
+                {
+                    string c = String.Format("{0}st century", rok);
+                    return c;
+                }
+                else if (year == 2)
+                {
+                    string c = String.Format("{0}nd century", rok);
+                    return c;
+                }
+                else if (year == 3)
+                {
+                    string c = String.Format("{0}rd century", rok);
+                    return c;
+                }
+                else
+                {
+                    string f = String.Format("{0}th century", rok);
+                    return f;
+                }
+
+
+
+            }
+            else
+            {
+                int rok = year / 100 + 1;
+
+                if (year == 1)
+                {
+                    string c = String.Format("{0}st century", rok);
+                    return c;
+                }
+                else if (year == 2)
+                {
+                    string c = String.Format("{0}nd century", rok);
+                    return c;
+                }
+                else if (year == 3)
+                {
+                    string c = String.Format("{0}rd century", rok);
+                    return c;
+                }
+                else
+                {
+                    string b = String.Format("{0}th century", rok);
+                    return b;
+                }
+                
+            }
+                
         }
     }
 }
+                           
